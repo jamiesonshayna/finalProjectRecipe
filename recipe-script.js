74bc3058c93b339e04ce26b69b1d8f03a7ddfd8e
Shayna Jamieson <sjamieson2@mail.greenriver.edu>
2019-03-12 16:57:12 -0700

    Finished JS testing statements

--- recipe-script.js
+++ recipe-script.js
@@ -2,28 +2,74 @@ let values = () => {
     $.getJSON('recipieTry1.json', function(result){
         $.each(result, function(index, choice){
                 /* get input min and max calories */
-            let userLowCal = document.getElementById('calLow').value;
             let userHighCal = document.getElementById('calHigh').value;
+            // let userLowCal = document.getElementById('calLow').value;
+            
+            if(userHighCal < 500) {
+                var validLowCal = true;
+            }
+            if(userHighCal > 500) {
+                var validLowCal = false;
+            }
             
                 /* get radio button checked 'on' or not */
             let radioVegan = document.getElementById("veganRad");
             let radioCarb = document.getElementById("lowCarbRad");
-            
-            
-            if(radioVegan.checked && userLowCal < 500) {
-                if(choice.vegan == "true" && choice.lowCalories == "true") {
-                $('#results').html(choice.recipeName+" "+choice.directions);
-                }
+            let radioSugar = document.getElementById("lowSugarRad");
+            let radioGluten = document.getElementById("glutenFreeRad");
+            let radioDairy = document.getElementById("dairyFreeRad");
+            let radioProtein = document.getElementById("highProteinRad");
+
+            // get the output so that it can be reused with just a variable
+            let output = "<h3>Recipe: "+choice.recipeName+"<br>Directions: "+choice.directions+"</h3>";
+
+            if(radioVegan.checked && validLowCal == true) {
+                if(choice.vegan == "true" && choice.lowCalories == "true")
+                    $('#results').html(output);
+            } 
+            else if(radioVegan.checked && validLowCal == false) {
+                if(choice.vegan == "true" && choice.lowCalories == "false")
+                    $('#results').html(output);
             }
-            if(radioVegan.checked && userLowCal > 500) {
-                if(choice.vegan == "true" && choice.lowCalories == "false") {
-                $('#results').html(choice.recipeName+" "+choice.directions);
-                }
+            else if(radioCarb.checked && validLowCal == true) {
+                if(choice.lowCarb == "true" && choice.lowCalories == "true")
+                    $('#results').html(output);
             }
-            if(radioCarb.checked && userLowCal < 500) {
-                if(choice.lowCarb == "true" && choice.lowCalories == "true") {
-                $('#results').html(choice.recipeName+" "+choice.directions);
-                }
+            else if(radioCarb.checked && validLowCal == false) {
+                if(choice.lowCarb == "true" && choice.lowCalories == "false")
+                    $('#results').html(output);
+            }
+            else if(radioSugar.checked && validLowCal == true) {
+                if(choice.lowSugar == "true" && choice.lowCalories == "true")
+                    $('#results').html(output);
+            }
+            else if(radioSugar.checked && validLowCal == false) {
+                if(choice.lowSugar == "true" && choice.lowCalories == "false")
+                    $("#results").html(output);
+            }
+            else if(radioGluten.checked && validLowCal == true) {
+                if(choice.glutenFree == "true" && choice.lowCalories == "true")
+                    $('#results').html(output);
+            }
+            else if(radioGluten.checked && validLowCal == false) {
+                if(choice.glutenFree == "true" && choice.lowCalories == "false")
+                    $('#results').html(output);
+            }
+            else if(radioDairy.checked && validLowCal == true) {
+                if(choice.dairyFree == "true" && choice.lowCalories == "true")
+                    $('#results').html(output);
+            }
+            else if(radioDairy.checked && validLowCal == false) {
+                if(choice.dairyFree == "true" && choice.lowCalories == "false")
+                    $('#results').html(output);
+            }
+            else if(radioProtein.checked && validLowCal == true) {
+                if(choice.highProtein == "true" && choice.lowCalories == "true")
+                    $('#results').html(output);
+            }
+            else if(radioProtein.checked && validLowCal == false) {
+                if(choice.highProtein == "true" && choice.lowCalories == "false")
+                    $('#results').html(output);
             }
         });
     });
